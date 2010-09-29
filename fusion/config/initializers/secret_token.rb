54862ab5fb865c90ce9b460d7533c83395f845a0
PROD <prod@prodfusion.(none)>
2010-09-29 23:24:32 +0000

    initial commit

---
+++ fusion/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fusion::Application.config.secret_token = 'f15a0c9ce4f986c05bf36d1e97b2d81fb8545eb9a495a663424ef031496e14a0d30b6c18bbd80c6061c2f87625eb55656f1a061ec24e3a733a0268ec4b934a9f'
