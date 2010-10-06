baeab25c9dd5a1390c937e0c2308c6502bb3815a
Ellis Berner <eberner@gmail.com>
2010-10-06 10:47:48 -0400

    fixed typo in unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,19 @@
 worker_processes 2
 working_directory "/home/prod/fusion/current"
-listen "/tmp/fusion.sock", :backlog => 64
+
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
+
 timeout 30
-user 'prod', 'staff'
-shared_path = "/home/prod/fusion/shared"
-pid = "#{shared_path}/pids/unicorn.pid"
-stderr_path = "#{shared_path}/log/unicorn.stderr.log"
-stdout_path = "#{shared_path}/log/unicorn.stdout.log"
+
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+listen "/tmp/fusion.sock", :backlog => 64
+
+pid "/home/prod/fusion/shared/pids/unicorn.pid"
+
+# Set the path of the log files inside the log folder of the testapp
+stderr_path "/home/prod/fusion/shared/log/unicorn.stderr.log"
+stdout_path "/home/prod/fusion/shared/log/unicorn.stdout.log"
