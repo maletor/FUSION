f07e61c5973e3f1418bfa7bc0326fc467c7733cf
Ellis Berner <eberner@gmail.com>
2010-12-09 17:39:06 -0500

    google

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
   $('h3.ui-state-default').addClass("ui-state-hover");
 
-  $('.graphic, .ui-icon').hover( 
+  $('.graphic, span.ui-icon').hover( 
     function () {
       $(this).addClass("ui-state-hover");    
     },
@@ -69,4 +69,14 @@ fx.elem.ias.update();
                                  }
              })
 });
+
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12479930-5']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
